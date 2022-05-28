f416a4e278ec28f2c0f1fa6cce7a85c47abe83bd
Joachim Heck <joachim@heckcorp.org>
2022-05-28 05:52:05 -0600

    Pulled ViewMultiplexer out into a top-level class.

--- src/org/heckcorp/domination/DefaultModel.java
+++ src/org/heckcorp/domination/DefaultModel.java
@@ -10,7 +10,6 @@ import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
 import java.io.Serializable;
 import java.util.ArrayList;
-import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
 import java.util.logging.Logger;
@@ -99,86 +98,6 @@ public class DefaultModel implements GameModel, Serializable {
         }
     }
 
-    public class ViewMultiplexer implements GameView {
-        /**
-         * @pre piece != null
-         * @pre piece.getPosition != null
-         */
-        public void addGamePiece(GamePiece piece) {
-            for (GameView view : gameViews) {
-                view.addGamePiece(piece);
-            }           
-        }
-
-        public void addGameView(GameView view) {
-            gameViews.add(view);
-        }
-
-        public void attack(Unit attacker, Unit target) {
-            for (GameView view : gameViews) {
-                view.attack(attacker, target);
-            }
-        }
-
-        public void message(String message) {
-            for (GameView view : gameViews) {
-                view.message(message);
-            }
-        }
-
-        public void move(Unit unit, Direction direction) {
-            for (GameView view : gameViews) {
-                log.finer(view + ".move(" + unit + "," + direction + ")");
-                view.move(unit, direction);
-            }
-        }
-
-        public void selectHex(Hex hex) {
-            for (GameView view : gameViews) {
-                view.selectHex(hex);
-            }
-        }
-
-        public void setCurrentPlayer(String playerName) {
-            for (GameView view : gameViews) {
-                view.setCurrentPlayer(playerName);
-            }
-        }
-
-        public void setMap(HexMap map) {
-            for (GameView view : gameViews) {
-                view.setMap(map);    
-            }
-        }
-
-        public void setMonitor(ViewMonitor monitor) {
-            for (GameView view : gameViews) {
-                view.setMonitor(monitor);
-            }
-        }
-
-        public void setShadowMap(ShadowMap shadowMap) {
-//            for (GameView view : gameViews) {
-//                view.setShadowMap(shadowMap);
-//            }
-        }
-
-        public void setStatus(Unit unit, Status status) {
-            for (GameView view : gameViews) {
-                log.finer("Setting status for view " + view);
-                view.setStatus(unit, status);
-            }
-        }
-
-        // TODO: remove?
-        public void setWinningPlayer(String playerName, Color playerColor) {
-            for (GameView view : gameViews) {
-                view.setWinningPlayer(playerName, playerColor);
-            }
-        }
-
-        private transient Set<GameView> gameViews = new HashSet<GameView>();
-    }
     /**
      * Adds the piece to the model at the specified position.
      * @param piece
