638a4217ae3c9a8822980c100311ef9189509159
Joachim Heck <joachim@heckcorp.org>
2022-06-08 07:50:20 -0600

    Fixed the nullness issues by removing all the @DefaultQualifier annotations and letting the checkerframework do its normal thing.

--- src/org/heckcorp/spacegame/model/package-info.java
+++
@@ -1,5 +0,0 @@
-//@DefaultQualifier(value = NotNull.class)
-package org.heckcorp.spacegame.model;
-
-import org.checkerframework.framework.qual.DefaultQualifier;
-import org.jetbrains.annotations.NotNull;
