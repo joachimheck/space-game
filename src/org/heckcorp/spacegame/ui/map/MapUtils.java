f7553ac83bf0613fcc5737020f2f84c86aeff8ef
Joachim Heck <joachim@heckcorp.org>
2022-06-11 06:47:57 -0600

    Updated the hexes to draw with computed coordinate values instead of constant ones.

--- src/org/heckcorp/spacegame/ui/map/MapUtils.java
+++ src/org/heckcorp/spacegame/ui/map/MapUtils.java
@@ -6,46 +6,43 @@ import javafx.scene.shape.Polygon;
 public class MapUtils {
 
   Point2D getHexCorner(Point position) {
-    double pixelX = position.x() * columnWidth;
-    double pixelY = position.y() * hexHeight;
+    double pixelX = position.x() * getColumnWidth();
+    double pixelY = position.y() * getHexHeight();
     if (position.x() % 2 != 0) {
-      pixelY += hexHeight / 2;
+      pixelY += getHexHeight() / 2;
     }
     return new Point2D(pixelX, pixelY);
   }
 
   /** Returns the pixel coordinates of the center of the hex with the specified map coordinates. */
   public Point2D getHexCenter(Point position) {
-    double pixelX = position.x() * columnWidth;
-    double pixelY = position.y() * hexHeight;
+    double pixelX = position.x() * getColumnWidth();
+    double pixelY = position.y() * getHexHeight();
     if (position.x() % 2 != 0) {
-      pixelY += hexHeight / 2;
+      pixelY += getHexHeight() / 2;
     }
-    return new Point2D(pixelX + hexWidth / 2.0, pixelY + hexHeight / 2.0);
+    return new Point2D(pixelX + getHexWidth() / 2.0, pixelY + getHexHeight() / 2.0);
   }
 
   public Polygon getHexagon(Point hexCoordinates) {
-    Point2D corner = getHexCorner(hexCoordinates);
+    Point2D center = getHexCenter(hexCoordinates);
+    double minorRadius = hexRadius * Math.sqrt(3) / 2.0;
+    double halfRadius = hexRadius / 2.0;
     return new Polygon(
-        corner.getX() + 32d,
-        corner.getY(),
-        corner.getX() + 96d,
-        corner.getY(),
-        corner.getX() + 128d,
-        corner.getY() + 55d,
-        corner.getX() + 96d,
-        corner.getY() + 110d,
-        corner.getX() + 32d,
-        corner.getY() + 110d,
-        corner.getX(),
-        corner.getY() + 55d);
+        center.getX() - halfRadius,
+        center.getY() - minorRadius,
+        center.getX() + halfRadius,
+        center.getY() - minorRadius,
+        center.getX() + hexRadius,
+        center.getY(),
+        center.getX() + halfRadius,
+        center.getY() + minorRadius,
+        center.getX() - halfRadius,
+        center.getY() + minorRadius,
+        center.getX() - hexRadius,
+        center.getY());
   }
 
-  /**
-   * @param canvasPoint the pixel coordinates of the point in this component's.
-   * @return the map coordinates of a hex that contains the specified point, or null if the clicked
-   *     point is not in a hex.
-   */
   public Point getHexCoordinates(Point2D canvasPoint) {
     Point[] guesses = guessHex(canvasPoint);
 
@@ -63,13 +60,12 @@ public class MapUtils {
     return new Point(closest.x(), closest.y());
   }
 
-  /**
-   * Returns three sets of viewport coordinates, one of which corresponds to the clicked-on hex.
-   */
-  private Point[] guessHex(Point2D screenPoint) {
-    int columnGuess = (int) (screenPoint.getX() / columnWidth);
+  /** Returns three sets of canvas coordinates, one of which corresponds to the clicked-on hex. */
+  private Point[] guessHex(Point2D canvasPoint) {
+    int columnGuess = (int) (canvasPoint.getX() / getColumnWidth());
     int rowShift = columnGuess % 2 != 0 ? 1 : 0;
-    int rowGuess = (int) ((screenPoint.getY() - (rowShift * hexHeight / 2.0)) / hexHeight);
+    int rowGuess =
+        (int) ((canvasPoint.getY() - (rowShift * getHexHeight() / 2.0)) / getHexHeight());
 
     Point[] guesses = new Point[3];
     guesses[0] = new Point(columnGuess, rowGuess);
@@ -79,13 +75,21 @@ public class MapUtils {
     return guesses;
   }
 
-  public MapUtils(double hexRadius) {
-    this.hexHeight = hexRadius * Math.sqrt(3);
-    this.columnWidth = hexRadius * 3.0 / 2.0;
-    this.hexWidth = 2.0 * hexRadius;
+  public double getColumnWidth() {
+    return hexRadius * 3.0 / 2.0;
   }
 
-  private final double hexHeight;
-  private final double hexWidth;
-  private final double columnWidth;
+  public double getHexHeight() {
+    return hexRadius * Math.sqrt(3);
+  }
+
+  public double getHexWidth() {
+    return 2.0 * hexRadius;
+  }
+
+  public MapUtils(double hexRadius) {
+    this.hexRadius = hexRadius;
+  }
+
+  private final double hexRadius;
 }
